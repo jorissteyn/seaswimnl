23bb5cffca3228d66799895b86cd7df23cbaaf47
Joris Steyn <joris@j0r1s.nl>
2025-12-20 21:58:13 +0100

    Fix Kernel namespace from App to Seaswim

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App;
+namespace Seaswim;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
